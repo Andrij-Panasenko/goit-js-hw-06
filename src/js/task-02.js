a316d11f0815eccef7cd30e24a4830f5a5038c7d
Andrij-Panasenko <andrii.panasenko295@gmail.com>
2023-08-16 21:58:07 +0300

    Task 2 and 3

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,18 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const list = document.getElementById("ingredients");
+console.dir(list);
+
+const markup = ingredients.map((item) => {
+  const li = document.createElement("li");
+  li.classList.add("item")
+  li.textContent = item;
+  list.append(li);
+});
