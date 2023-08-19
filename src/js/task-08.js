7face09d690612704e5dfa26be4c565be7355150
Andrij-Panasenko <andrii.panasenko295@gmail.com>
2023-08-20 00:30:46 +0300

    Task 8 - login form submit

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,31 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    
+    const form = event.currentTarget;
+    const formData = new FormData(event.currentTarget);
+    const formObject = {};
+    
+    formData.forEach((value, name) => {
+        formObject[name] = value;
+    })
+
+    if (validateForm(formObject)) {
+        console.log(formObject);
+        form.reset()
+    } else {
+        alert('All fields must be fillded')
+    }
+}
+
+function validateForm(data) {
+    for (const key in data) {
+        if (data[key] === '') {
+            return false
+        }
+    }
+    return true;
+}
