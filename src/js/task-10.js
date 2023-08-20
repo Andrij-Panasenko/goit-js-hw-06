ca235a02932609c299b5f154e35703955e060f94
Andrij-Panasenko <andrii.panasenko295@gmail.com>
2023-08-20 21:36:41 +0300

    Update task-10.js

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -5,13 +5,30 @@ function getRandomHexColor() {
 }
 
 
+const boxesContainer = document.querySelector("#boxes");
+const boxesAmount = document.querySelector('input[type="number"]');
+const createBtn = document.querySelector('[data-create]');
+const destroyBtn = document.querySelector("[data-destroy]");
+
+createBtn.addEventListener("click", createBoxes);
+destroyBtn.addEventListener("click", destroyBoxes);
 
 
 
+function createBoxes() { 
+  const amount = boxesAmount.value;
+  boxesContainer.innerHTML = '';
 
+  for (let i = 0; i < amount; i++) {
+    const size = 30 + i * 10;
+    const box = document.createElement("div");
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxesContainer.append(box);
+  }
+};
 
-
-
-
-function createBoxes(amount) { }
-function destroyBoxes() { }
+function destroyBoxes() {
+  boxesContainer.innerHTML = '';
+};
